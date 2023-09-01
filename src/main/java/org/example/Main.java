dacd5f253e3cc67e1ee47b5a90dd0c7ef052a94a
Gorytitan <76826556+Gorytitan@users.noreply.github.com>
2023-09-01 16:25:36 -0500

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,105 @@
+package org.example;
+
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //https://api.warframe.market/v1
+        try {
+            URL url = new URL("https://api.warframe.market/v1/items/primed_fulmination/orders");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Check if connect is made
+            int responseCode = conn.getResponseCode();
+
+            //200 ok
+            if (responseCode != 200)
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            else {
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()){
+                    informationString.append(scanner.nextLine());
+                }
+                scanner.close();
+                conn.disconnect();
+
+                //turn the string into a json
+                JSONParser parse = new JSONParser();
+                JSONObject dataObject = (JSONObject) parse.parse(String.valueOf(informationString));
+                JSONArray arr = (JSONArray) ((JSONObject) dataObject.get("payload")).get("orders");
+
+                //sorting by price for easier access
+                Collections.sort(arr, new Comparator<JSONObject>(){
+                    @Override
+                    public int compare(JSONObject o1, JSONObject o2) {
+                        return ((Long) o1.get("platinum")).intValue() - ((Long) o2.get("platinum")).intValue();
+                    }
+                });
+
+                //delete old posts (might not be needed if i just do online ones
+                DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+                DateTime now = new DateTime();
+                DateTime old = now.minusDays(30);
+                for (int i = arr.size()- 1; i >= 0; i--){
+                    DateTime dt = formatter.parseDateTime(((JSONObject)arr.get(i)).get("last_update").toString());
+                    if (dt.isBefore(old)){
+                        arr.remove(i);
+                    }
+                }
+
+                //seperate into 2 arrays, one for unranked one for maxed
+                JSONArray unranked = new JSONArray();
+                JSONArray maxed = new JSONArray();
+                for (Object order:
+                     arr) {
+                    JSONObject orderjson = (JSONObject) order;
+                    switch (((Long) orderjson.get("mod_rank")).intValue()){
+                        case 0:
+                            if(orderjson.get("order_type").toString().equals("buy")
+                                    && ( ( ( (JSONObject) orderjson.get("user")).get("status").toString().equals("online" ))
+                                    || ( (JSONObject) orderjson.get("user")).get("status").toString().equals("ingame"))) {
+                                unranked.add(orderjson);
+                            }
+                            break;
+                        case 10:
+                            if(orderjson.get("order_type").toString().equals("sell")
+                                    && (( ((JSONObject) orderjson.get("user")).get("status").toString().equals("online"))
+                                    || ((JSONObject) orderjson.get("user")).get("status").toString().equals("ingame"))){
+                                maxed.add(orderjson);
+                            }
+                            break;
+                    }
+                }
+                System.out.println(maxed);
+                /*TODO  i seperated them into wts maxed and wtb unranked, take average of the 3 cheapest each and find the difference. Make a class for a mod with this code inside of it then
+                  TODO another method to pull information from a spread sheet to do this for each mod. Make a compare method for the mod class and then just collections.sort lol
+                  TODO then output that in a new spreadsheet?
+                */
+            }
+
+        } catch (Exception e){
+            System.out.print(e);
+        }
+
+    }
+    private static int price (Object a, Object b){
+        return 0;
+    }
+}
