7a65cb12259ff1bb93c8c3529b8e4ed03092fb35
Gorytitan <76826556+Gorytitan@users.noreply.github.com>
2023-09-13 12:38:08 -0500

    removed uneccessary stuff from mod/arcane to just arcane

--- src/main/java/org/tama/marketskimmer/ApiCaller.java
+++ src/main/java/org/tama/marketskimmer/ApiCaller.java
@@ -38,33 +38,5 @@ public class ApiCaller {
 
         return informationString;
     }
-
-    /**
-     * Converts a string json given by an API to a JSONObject from the json.simple library
-     *
-     * @param apiurl The URL string of the API you're pulling the JSON from
-     * @return JSONObject based on the URL provided
-     * @throws IOException Bad url or network issues can cause this
-     * @throws ParseException if the url does not link to a JSONFile this will happen
-     */
-    public static JSONObject getJsonObject(String apiurl) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
-        StringBuilder json = getJSON(apiurl);
-        return (JSONObject) parser.parse(String.valueOf(json));
-    }
-
-    /**
-     * Converts a string json given by an API to a JSONARRAY from the json.simple library
-     *
-     * @param apiurl The URL string of the API you're pulling the JSON from
-     * @return JSONArray based on the URL provided
-     * @throws IOException Bad url or network issues can cause this
-     * @throws ParseException if the URL does not lead to a JSON this will be thrown
-     */
-    public static JSONArray getJsonArray(String apiurl) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
-        StringBuilder json = getJSON(apiurl);
-        return (JSONArray) parser.parse(String.valueOf(json));
-    }
 }
 
--- src/main/java/org/tama/marketskimmer/Market.java
+++ src/main/java/org/tama/marketskimmer/Market.java
@@ -10,45 +10,6 @@ import java.util.Date;
 public class Market {
     private static final int MARKET_DATA_DURATION_DAYS = 7;
 
-    private static JSONArray getModStatsJson(String modUrlName) throws IOException {
-        StringBuilder jsonStringBuilder = ApiCaller.getJSON("https://api.warframe.market/v1/items/" + modUrlName + "/statistics");
-        //turn the string into a json
-        JSONObject wrapper;
-        try {
-            JSONParser parse = new JSONParser();
-            wrapper = (JSONObject) parse.parse(String.valueOf(jsonStringBuilder));
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
-        }
-        JSONObject payload = (JSONObject) wrapper.get("payload");
-        JSONObject statisticsClosed = (JSONObject) payload.get("statistics_closed");
-        return (JSONArray) statisticsClosed.get("90days");
-    }
-
-    public static double[] getModStats(String modUrlName) throws IOException {
-        JSONArray stats = getModStatsJson(modUrlName);
-        if (stats == null) return new double[]{0, 0};
-        double sumMax = 0;
-        double sumUnranked = 0;
-        double volMax = 0;
-        double volUnranked = 0;
-        for (Object stat : stats) {
-            JSONObject statJson = (JSONObject) stat;
-            Date dateDaysFromToday = DateManagement.subtractDaysFromToday(MARKET_DATA_DURATION_DAYS);
-            Date dateFromJson = DateManagement.extractDateFromJson(((JSONObject) stat).get("datetime").toString());
-            if (dateDaysFromToday.after(dateFromJson)) continue;
-            if (Double.parseDouble(statJson.get("mod_rank").toString()) == 0) {
-                sumUnranked += Double.parseDouble(statJson.get("wa_price").toString()) * Double.parseDouble(statJson.get("volume").toString());
-                volUnranked += Double.parseDouble(statJson.get("volume").toString());
-            } else {
-                sumMax += Double.parseDouble(statJson.get("wa_price").toString()) * Double.parseDouble(statJson.get("volume").toString());
-                volMax += Double.parseDouble(statJson.get("volume").toString());
-            }
-        }
-        return new double[]{sumMax / Math.max(volMax, 1) - sumUnranked / Math.max(volUnranked, 1), volMax};
-    }
-
     public static double[] getArcaneStats(String urlName) throws IOException {
         StringBuilder jsonStringBuilder = ApiCaller.getJSON("https://api.warframe.market/v1/items/" + urlName + "/statistics");
         //turn the string into a json
@@ -75,7 +36,7 @@ public class Market {
                 volume += Integer.parseInt(statJson.get("volume").toString());
             }
         }
-        return new double[] {sumPrice / volume, volume};
+        return new double[]{sumPrice / volume, volume};
 
     }
 }
